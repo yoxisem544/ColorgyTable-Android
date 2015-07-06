2afaac1eb85c450fc228a30fe12589b28c060a0b
David <yoxisem544@gmail.com>
2015-07-06 23:14:20 +0800

    add fb login, but with bugs.

--- www/js/app.js
+++ www/js/app.js
@@ -3,13 +3,15 @@
 // angular.module is a global place for creating, registering and retrieving Angular modules
 // 'starter' is the name of this angular module example (also set in a <body> attribute in index.html)
 // the 2nd parameter is an array of 'requires'
-angular.module('colorgytable', ['ionic', 'colorgytable.controllers'])
+angular.module('colorgytable', ['ionic', 'colorgytable.controllers', 'ngOpenFB'])
 
-.run(function($ionicPlatform) {
+.run(function($ionicPlatform, ngFB) {
   $ionicPlatform.ready(function() {
     // Hide the accessory bar by default (remove this to show the accessory bar above the keyboard
     // for form inputs)
     console.log('colorgy table starting...');
+
+    ngFB.init({appId: '752440461479659'});
 
     if(window.cordova && window.cordova.plugins.Keyboard) {
       cordova.plugins.Keyboard.hideKeyboardAccessoryBar(true);
--- www/js/viewcontrollers.js
+++ www/js/viewcontrollers.js
@@ -1,9 +1,10 @@
-angular.module('colorgytable.controllers', [])
+angular.module('colorgytable.controllers', ['ngOpenFB'])
 
-.controller('MenuCtrl', function($scope, $ionicModal) {
+.controller('MenuCtrl', function($scope, $ionicModal, ngFB) {
 
   // detect if user is logged in.
   if (!window.localStorage['isLogin']) {
+    console.log("user login state: not yet logged in.");
     $ionicModal.fromTemplateUrl('views/login.html', {
       scope: $scope,
       backdropClickToClose: false
@@ -12,17 +13,33 @@ angular.module('colorgytable.controllers', [])
       // if modal is ready to show, show it.
       $scope.modal.show();
     });
+  } else {
+    console.log("user login state: user logged in.");
   }
 
   // this region is login logic....
+  $scope.logindata = {};
   $scope.closeButton = function() {
-    console.log("click..");
+    console.log("close button clicked..");
     $scope.modal.hide();
   };
 
   $scope.doLogin = function() {
     console.log('do login');
+    console.log('u: ' + $scope.logindata.username + ', p: ' + $scope.logindata.password);
+  };
 
+  // fb region
+  $scope.fbLogin = function() {
+    ngFB.login({scope: 'email'}).then(
+        function (response) {
+            if (response.status === 'connected') {
+                console.log('Facebook login succeeded');
+                $scope.closeLogin();
+            } else {
+                alert('Facebook login failed');
+            }
+        });
   };
 
 })
